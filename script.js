5f9a67f0a91dce3f705d85399b0aee6b9daf5abb
Facundo Andrés De Los Rios <facujofre96@gmail.com>
2022-09-04 13:06:45 -0300

    clase04 completada

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+( () => {
+
 const btn = document.querySelector('[data-form-btn]'); //Seleccionamos el boton del form
 
 const createTask = (evento) => {
@@ -8,21 +10,34 @@ const createTask = (evento) => {
     const list =  document.querySelector('[data-list]'); //Seleccionamos el ul
     const task = document.createElement('li'); //creamos un li dentro de [data-list]
     task.classList.add('card');
-    
     //backticks ``
-    const content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-        </div>
+    const taskContent = document.createElement('div'); //Creamos un elemento del tipo div
+    taskContent.appendChild(checkComplete()); // Agregamos a checkComplete (checkbox) como hijo de taskContent (div)
+    const titileTask = document.createElement('span'); //Creamos un elemento del tipo span
+    titileTask.classList.add('task'); // agregamos la clase al titletask (span)
+    titileTask.innerText = value; //Luego le agregamos el valor de contenido (texto) al span (titleTask)
+    taskContent.appendChild(titileTask); // Agregamos a titleTask (span) como hijo de taskContent (div)
+    const content = `
         <i class="fas fa-trash-alt trashIcon icon"></i>`
-    task.innerHTML = content; //agrergamos el contenido dentro del task
-    
-    list.appendChild(task);
-    
-    console.log(content);
+   // task.innerHTML = content; //agrergamos el contenido dentro del task
+    task.appendChild(taskContent); 
+    list.appendChild(task); //Agregamos un li (hijo) dentro del ul (padre)
 }
-
-console.log(btn);
-
 //Usando Arrow function o funciones anonimas
 btn.addEventListener('click', createTask);
+//Creamo funcion anonima para el checkbox del formulario
+const checkComplete = () => {
+    const i = document.createElement('i');
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+};
+// Inmediatly invoked function expression (IIFE), es decir funciones que en cuanto se declaran se ejecutan
+const completeTask = (event) => {
+    const element = event.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon')
+    element.classList.toggle('far');
+};
+
+}) ();
