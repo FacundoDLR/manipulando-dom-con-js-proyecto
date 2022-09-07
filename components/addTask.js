3a33f633d9bf9b6c5bc426c3a31b8cb951d10ab8
Facundo Andrés De Los Rios <facujofre96@gmail.com>
2022-09-07 15:05:53 -0300

    borrando tareas del local storage

--- components/addTask.js
+++ components/addTask.js
@@ -20,9 +20,13 @@ export const addTask = (evento) => {
     input.value = ''; //Luego de agregar la tarea dejamos el campo del input vacio
     calendar.value = '';
 
+    const complete = false;
+
     const taskObj = { //generamos una constante, que es un objeto en el cual vamos a almacenar tanto value como dateFormat.
         value,
-        dateFormat
+        dateFormat,
+        complete,
+        id: uuid.v4()
     } //Creamos un objeto que contiene llave y valor, y que servirá para almacenar una serie de datos en el LocalStorage.
     
 list.innerHTML = ''; //por cada una de las tareas que se están agregando, cada vez que nosotros estamos dando clic en el botón, lo que va a hacer es inicializar o decir que su estructura es cero, es simplemente un string vacío.
@@ -34,22 +38,29 @@ list.innerHTML = ''; //por cada una de las tareas que se están agregando, cada 
     displayTasks(); //displayTask se va a encargar de ir agregando cada una de las tareas
 }
 
-export const createTask = ({value, dateFormat}) => {
+export const createTask = ({value, dateFormat, complete, id}) => {
     const task = document.createElement('li'); //creamos un li dentro de [data-list]
             task.classList.add('card'); //agregamos una clase llamada card
     //backticks ``
     const taskContent = document.createElement('div'); //Creamos un elemento del tipo div
 
+    const check = checkComplete(id);
+
+    if(complete) {
+        check.classList.toggle('fas');
+        check.classList.toggle('completeIcon')
+        check.classList.toggle('far');
+    }
     const titileTask = document.createElement('span'); //Creamos un elemento del tipo span
             titileTask.classList.add('task'); // agregamos la clase al titletask (span)
             titileTask.innerText = value; //Luego le agregamos el valor de contenido (texto) al span (titleTask)
-            taskContent.appendChild(checkComplete()); // Agregamos a checkComplete (checkbox) como hijo de taskContent (div)
+            taskContent.appendChild(check); // Agregamos a checkComplete (checkbox) como hijo de taskContent (div)
             taskContent.appendChild(titileTask); // Agregamos a titleTask (span) como hijo de taskContent (div)
     // task.innerHTML = content;           //agrergamos el contenido dentro del task
     const dateElement = document.createElement('span');
             dateElement.innerHTML = dateFormat;
             task.appendChild(taskContent);
             task.appendChild(dateElement); 
-            task.appendChild(deleteIcon());
+            task.appendChild(deleteIcon(id));
     return task;
 }
